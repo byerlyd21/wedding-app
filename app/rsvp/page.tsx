b6f4de2e752476f44117b6801885ec458a2cc393
byerlyd21 <dallinbyerly@gmail.com>
2025-03-17 08:57:30 -0600

    styling fix

--- app/rsvp/page.tsx
+++ app/rsvp/page.tsx
@@ -74,74 +74,76 @@ export default function RSVPPage() {
 
   return (
     <>
-    <div className="rsvp-container">
-        {submitted ? (
-        <div className="flex flex-col items-center">
-            <h1 className="header-primary mt-24">Thank you!</h1>
-            <p>We look forward to seeing you!</p>
-            <a
-                href="https://amazon.com"
-                className="btn-primary mt-12!"
-                >
-                View our registry
-            </a>
-        </div>
-        ) : (
-        <>
-        <div className="rsvp-header">
-            <h1 className="header-primary">Reserve your spot!</h1>
-            <p className="text-secondary">We'd love to see you at our wedding on August 16th at 5:00 pm</p>
-        </div>
-            <form onSubmit={handleSubmit} className="rsvp-form">
-                <label className="text-secondary w-full">Name</label>
-                <input
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    className="rsvp-input"
-                />
-                <label className="text-secondary w-full">Phone</label>
-                <div className="phone-input-container">
-                    <InputOTP maxLength={10} value={phone} onChange={handleOTPChange}>
-                        <InputOTPGroup className="phone-group-3">
-                            <InputOTPSlot index={0} className="phone-input"/>
-                            <InputOTPSlot index={1} className="phone-input"/>
-                            <InputOTPSlot index={2} className="phone-input"/>
-                        </InputOTPGroup>
-                        <InputOTPGroup className="phone-group-3">
-                            <InputOTPSlot index={3} className="phone-input"/>
-                            <InputOTPSlot index={4} className="phone-input"/>
-                            <InputOTPSlot index={5} className="phone-input"/>
-                        </InputOTPGroup>
-                        <InputOTPGroup className="phone-group-4">
-                            <InputOTPSlot index={6} className="phone-input"/>
-                            <InputOTPSlot index={7} className="phone-input"/>
-                            <InputOTPSlot index={8} className="phone-input"/>
-                            <InputOTPSlot index={9} className="phone-input"/>
-                        </InputOTPGroup>
-                    </InputOTP>
-                </div>
-                <label className="text-secondary w-full">Address</label>
-                <input
-                    type="text"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                    className="rsvp-input"
-                />
-                {nameError && <span className="text-red-500 text-sm">{nameError}</span>}
-                <button type="submit" className="btn-primary" disabled={loading}>
-                {loading ? (
-                    <>
-                        <div className="spinner"></div>
-                        Submitting...
-                    </>
-                ) : (
-                    "Submit RSVP"
-                )}
-                </button>
-            </form>
-        </>
-        )}
+    <div className={submitted ? "border m-4" : ""}>
+      <div className="rsvp-container">
+          {submitted ? (
+          <div className="flex flex-col items-center">
+              <h1 className="header-primary mt-24">Thank you!</h1>
+              <p className="header-secondary">We look forward to seeing you!</p>
+              <a
+                  href="https://amazon.com"
+                  className="btn-primary mt-12!"
+                  >
+                  View our registry
+              </a>
+          </div>
+          ) : (
+          <>
+          <div className="rsvp-header">
+              <h1 className="header-primary">Reserve your spot!</h1>
+              <p className="text-secondary">We'd love to see you at our wedding on August 16th at 5:00 pm</p>
+          </div>
+              <form onSubmit={handleSubmit} className="rsvp-form">
+                  <label className="text-secondary w-full">Name</label>
+                  <input
+                      type="text"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      className="rsvp-input"
+                  />
+                  <label className="text-secondary w-full">Phone</label>
+                  <div className="phone-input-container">
+                      <InputOTP maxLength={10} value={phone} onChange={handleOTPChange}>
+                          <InputOTPGroup className="phone-group-3">
+                              <InputOTPSlot index={0} className="phone-input"/>
+                              <InputOTPSlot index={1} className="phone-input"/>
+                              <InputOTPSlot index={2} className="phone-input"/>
+                          </InputOTPGroup>
+                          <InputOTPGroup className="phone-group-3">
+                              <InputOTPSlot index={3} className="phone-input"/>
+                              <InputOTPSlot index={4} className="phone-input"/>
+                              <InputOTPSlot index={5} className="phone-input"/>
+                          </InputOTPGroup>
+                          <InputOTPGroup className="phone-group-4">
+                              <InputOTPSlot index={6} className="phone-input"/>
+                              <InputOTPSlot index={7} className="phone-input"/>
+                              <InputOTPSlot index={8} className="phone-input"/>
+                              <InputOTPSlot index={9} className="phone-input"/>
+                          </InputOTPGroup>
+                      </InputOTP>
+                  </div>
+                  <label className="text-secondary w-full">Address</label>
+                  <input
+                      type="text"
+                      value={address}
+                      onChange={(e) => setAddress(e.target.value)}
+                      className="rsvp-input"
+                  />
+                  {nameError && <span className="text-red-500 text-sm">{nameError}</span>}
+                  <button type="submit" className="btn-primary" disabled={loading}>
+                  {loading ? (
+                      <>
+                          <div className="spinner"></div>
+                          Submitting...
+                      </>
+                  ) : (
+                      "Submit RSVP"
+                  )}
+                  </button>
+              </form>
+          </>
+          )}
+      </div>
     </div>
     </>
   );
